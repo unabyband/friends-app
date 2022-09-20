2d068ed17e2963b1bbff7c6eee380b7724d984a8
unabyband <110461462+unabyband@users.noreply.github.com>
2022-09-20 10:52:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+const url = 'https://randomuser.me/api/?results=100&inc=gender,name,location,dob,picture,email'
+
+document.addEventListener('DOMContentLoaded', getUserData());
+
+let userStorage = [];
+let gender = 'all';
+let rule = 'none';
+
+async function getUserData() {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        data.results.map((user) => {
+            userStorage.push({
+                firstName: user.name.first,
+                lastName: user.name.last,
+                picture: user.picture.large,
+                age: user.dob.age,
+                gender: user.gender,
+                country: user.location.country,
+                city: user.location.city,
+                email: user.email,
+            });
+        });
+        renderToPage(userStorage);    
+    } catch (error) {
+        console.log(error);
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
+    }
+  }
+
+function createProfile(user) {
+    for(let i=0; i<user.length; i++) {
+        const card = document.querySelector('.usercards');
+        let userProfile = document.createElement('div');
+
+        userProfile.innerHTML = `<img class="user_pic" src="${user[i].picture}" alt="">
+            <div class="user_info">
+            <h1>${user[i].firstName} ${user[i].lastName}</h1>
+            <h2>${user[i].gender}, ${user[i].age}</h2>
+            <p>${user[i].city}, ${user[i].country}</p>
+            <span>${user[i].email}</span>
+            </div>`;
+        userProfile.classList.add("user_profile");
+        userProfile.classList.add(`${user[i].gender}`);
+        card.append(userProfile);
+    }
+}
+
+function renderToPage(user) {
+    document.querySelectorAll('.user_profile').forEach((element) => element.remove());
+    createProfile(user);
+}
+
+function filterUsers(user, gender) {
+    if(gender == 'male') {
+        return user.filter(({gender}) => gender == 'male');
+    } if (gender == 'female') {
+        return user.filter(({gender}) => gender == 'female');
+    } else {
+        return user;
+    }
+}
+
+function sortUsers(user, rule) {
+    if (rule == 'age_up') {
+        return user.sort(({age: a}, {age: b}) => a - b);   
+    } if (rule == 'age_down') {
+        return user.sort(({age: a}, {age: b}) => b - a);
+    } if (rule == 'names_up') {
+        return user.sort(({firstName: a}, {firstName: b}) => a < b ? -1 : 1); 
+    } if (rule == 'names_down') {
+        return user.sort(({firstName: a}, {firstName: b}) => a > b ? -1 : 1);
+    } else {
+        return user;
+    }
+}
+
+function findUsers(user, stringValue) {
+    if(parseInt(stringValue, 10)) {
+        return user.filter(({age}) => age == parseInt(stringValue, 10));
+    } else {
+    return user.filter(({firstName, lastName}) => (firstName + ' ' + lastName)
+    .toLowerCase().includes(stringValue.toLowerCase()));
+    }
+}
+
+
+const filtering = document.querySelectorAll('.gender_check');
+filtering.forEach((element) => element.addEventListener('change', showFilteredUsers));
+
+function showFilteredUsers() {
+    gender = this.value;
+    renderToPage(filterUsers((findUsers(userStorage, textValue)), gender));
+}
+
+const sorting = document.querySelectorAll('.sort_check');
+sorting.forEach((element) => element.addEventListener('change', showSortedUsers));
+
+function showSortedUsers() {
+    rule = this.value;
+    renderToPage(sortUsers((filterUsers((findUsers(userStorage, textValue)), gender)), rule));
+}
+
+let searching = document.querySelector('.text_check');
+searching = addEventListener('input', showFoundUsers);
+
+function showFoundUsers () {
+    textValue = document.getElementById('text_input').value;
+    renderToPage(findUsers(filterUsers(userStorage, gender), textValue));
+}
+
